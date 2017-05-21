677db9105deefd1cd5ab7cac161842536d00ec03
dance2die <dontbothermewithspam@gmail.com>
2017-05-21 14:57:00 -0400

    Added "pre-build" event for webpack.

---
+++ AspNetMvc5/Scripts/es6/main.js
@@ -0,0 +1,4 @@
+﻿import Person from './person';
+
+var person = new Person("David", 20);
+person.speak();
---
+++ AspNetMvc5/webpack.config.js
@@ -0,0 +1,18 @@
+﻿const path = require('path');
+
+module.exports = {
+	entry: ['./Scripts/es6/main.js'],
+	output: {
+		path: path.resolve(__dirname, './Scripts/build'),
+		filename: 'bundle.js'
+	},
+	module: {
+		loaders: [
+			{
+				loader: 'babel-loader',
+				test: /\.js$/,
+				exclude: /node_modules/
+			}
+		]
+	}
+}
