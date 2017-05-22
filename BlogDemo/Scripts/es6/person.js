1a7fca6e5ceba5a6abc7ed0e2738e3d65d23942f
dance2die <dontbothermewithspam@gmail.com>
2017-05-22 07:04:53 -0400

    blog demo works with es6

---
+++ BlogDemo/Scripts/es6/person.js
@@ -0,0 +1,10 @@
+﻿export default class Person {
+	constructor(name, age) {
+		this.name = name;
+		this.age = age;
+	}
+
+	speak() {
+		console.log(`Hi I'm ${this.name} and ${this.age} years old and I am awesome`);
+	}
+}
